dc63228bdf4b02077a2b00403673b4b876de4673
jimms-workspace-admin <klisdimitrios@gmail.com>
2025-04-01 03:01:19 +0300

    Added a bunch of UI stuff.

---
+++ School Timetable v2/Assets/Scripts/TabHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabHandler : MonoBehaviour
+{
+    [System.Serializable]
+    public class Tab
+    {
+        public Button TabButton;
+        public GameObject TabObject;
+    }
+    public Tab[] tabs;
+
+    public void SelectTab(int index)
+    {
+        if (index < 0 || index >= tabs.Length) return;
+        for (int i = 0; i < tabs.Length; i++)
+        {
+            tabs[i].TabObject.SetActive(index == i);
+            tabs[i].TabButton.interactable = index != i;
+        }
+    }
+    private void Start()
+    {
+        SelectTab(0);
+    }
+}
