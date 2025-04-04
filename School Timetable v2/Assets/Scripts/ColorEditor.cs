1449f47a953bc652365f63dc97ecb19a4e5b523d
jimms-workspace-admin <klisdimitrios@gmail.com>
2025-04-04 23:47:28 +0300

    Created a couple UI Prefabs for future use. Added HSV Color Editor.

---
+++ School Timetable v2/Assets/Scripts/ColorEditor.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ColorEditor : MonoBehaviour
+{
+    [Header("Basic References")]
+    public Slider HueSlider;
+    public Slider SaturationSlider;
+    public Slider ValueSlider;
+    [Space]
+    public Slider AlphaSlider;
+    [Space]
+    public TMP_InputField HexCodeField;
+    [Header("GFX References")]
+    public Image HueValImage; // The rainbow gradient
+    public Image HueSatImage; // The white overlay
+    [Space]
+    public Image SatValHueImage; // The main sat image
+    public Image SatValImage; // The child sat image
+    [Space]
+    public Image ValHueSatImage;
+    [Space]
+    public Image AlphaRGBImage;
+    [Space]
+    public Color FinalColor;
+    public Image ColorPreview;
+
+
+
+    public void UpdateColor()
+    {
+        FinalColor = Color.HSVToRGB(HueSlider.value, SaturationSlider.value, ValueSlider.value);
+        FinalColor.a = AlphaSlider.value;
+
+        // Setting the HUE Slider color
+        HueValImage.color = Color.HSVToRGB(0, 0, ValueSlider.value);
+
+        Color HueSat = HueSatImage.color;
+        HueSat.a = 1 - SaturationSlider.value;
+        HueSatImage.color = HueSat;
+
+        // Setting the SAT Slider color
+        SatValHueImage.color = Color.HSVToRGB(HueSlider.value, 1, ValueSlider.value);
+        SatValImage.color = Color.HSVToRGB(1, 0, ValueSlider.value);
+
+        // Setting the VAL Slider color
+        ValHueSatImage.color = Color.HSVToRGB(HueSlider.value,SaturationSlider.value, 1);
+
+        // Setting the ALPHA Slider color.
+        AlphaRGBImage.color = new(FinalColor.r, FinalColor.g, FinalColor.b, 1);
+
+        // Setting the Color Preview Color.
+        ColorPreview.color = FinalColor;
+
+        HexCodeField.text = ColorUtility.ToHtmlStringRGBA(FinalColor);
+    }
+    public void UpdateSliders()
+    {
+        float H, S, V;
+        Color.RGBToHSV(FinalColor, out H, out S, out V);
+        HueSlider.value = H;
+        SaturationSlider.value = S;
+        ValueSlider.value = V;
+        AlphaSlider.value = FinalColor.a;
+        UpdateColor();
+    }
+
+    public void SetColor(string hexCode)
+    {
+        if (!hexCode.Contains("#"))
+        {
+            hexCode = hexCode.Insert(0, "#");
+        }
+
+        Color c = FinalColor;
+        ColorUtility.TryParseHtmlString(hexCode, out c);
+        FinalColor = c;
+        UpdateSliders();
+    }
+}
---
+++ School Timetable v2/Assets/TextMesh Pro/HexCodeValidator.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "InputValidator - HexCodeValidator.asset", menuName = "TextMeshPro/Input Validators/HexCode", order = 100)]
+public class HexCodeValidator : TMP_InputValidator
+{
+    // Custom text input validation function
+    public override char Validate(ref string text, ref int pos, char ch)
+    {
+        bool validLetter = (ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'f') || (ch >= 'A' && ch <= 'F');
+        bool canHashtag = ch == '#' && pos == 0;
+
+        if ((validLetter || canHashtag) && pos <= 8)
+        {
+            text += ch;
+            pos += 1;
+            return ch;
+        }
+        return (char)0;
+    }
+    bool ContainsHashtag(string text)
+    {
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == '#') return true;
+        }
+        return false;
+    }
+}
